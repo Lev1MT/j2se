9f9fd8b28dc36bc7d147a5415c6eb553c85d4450
Lev1_MT <14108426@hdu.edu.cn>
2019-06-30 09:26:55 +0800

    spinLock

---
+++ src/com/lev1/interview/SpinLockDemo.java
@@ -0,0 +1,59 @@
+package com.lev1.interview;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * 自旋锁代码实现
+ * 自旋锁由于会不断的尝试获取锁，会耗费一定的cpu资源，所以谨慎使用
+ */
+public class SpinLockDemo {
+
+    // 原子引用线程
+    AtomicReference<Thread> atomicReference = new AtomicReference<>();
+
+    public void myLock(){
+        Thread thread = Thread.currentThread();
+        System.out.println(Thread.currentThread().getName()+"\t come in");
+        // 自旋等待
+        while (!atomicReference.compareAndSet(null,thread)){
+
+        }
+    }
+
+    public void myUnLock(){
+        Thread thread = Thread.currentThread();
+        System.out.println(Thread.currentThread().getName()+"\t invoked myUnLock()");
+        // 释放锁
+        atomicReference.compareAndSet(thread,null);
+    }
+
+    public static void main(String[] args) {
+        SpinLockDemo spinLockDemo = new SpinLockDemo();
+
+        new Thread(()->{
+            spinLockDemo.myLock();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            spinLockDemo.myUnLock();
+        },"AA").start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        new Thread(()->{
+            spinLockDemo.myLock();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            spinLockDemo.myUnLock();
+        },"BB").start();
+    }
+
+}
